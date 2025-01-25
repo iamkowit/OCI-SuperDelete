50d52acf4db54227c56088824ae6bcbc917a64f5
iamkowit <30616703+iamkowit@users.noreply.github.com>
2025-01-25 20:21:38 +0700

    Update delete.py

--- delete.py
+++ delete.py
@@ -290,17 +290,17 @@ if confirm == "yes":
         print_header("Deleting OKE Clusters at " + CurrentTimeString() + "@ " + region, 1)
         DeleteAny(config, signer, processCompartments, "container_engine.ContainerEngineClient", "cluster", ObjectNameVar="name")
 
-        print_header("Deleting Repositories at " + CurrentTimeString() + "@ " + region, 1)
-        DeleteAny(config, signer, processCompartments, "artifacts.ArtifactsClient", "container_repository", ServiceID="repository_id")
-        DeleteAny(config, signer, processCompartments, "artifacts.ArtifactsClient", "repository")
+        # print_header("Deleting Repositories at " + CurrentTimeString() + "@ " + region, 1)
+        # DeleteAny(config, signer, processCompartments, "artifacts.ArtifactsClient", "container_repository", ServiceID="repository_id")
+        # DeleteAny(config, signer, processCompartments, "artifacts.ArtifactsClient", "repository")
 
         print_header("Deleting Auto Scaling Configurations at " + CurrentTimeString() + "@ " + region, 1)
         DeleteAny(config, signer, processCompartments, "autoscaling.AutoScalingClient", "auto_scaling_configuration", DelState="", DelingSate="")
 
-        print_header("Deleting OS Management services at " + CurrentTimeString() + "@ " + region, 1)
-        DeleteAny(config, signer, processCompartments, "os_management.OsManagementClient", "managed_instance_group")
-        DeleteAny(config, signer, processCompartments, "os_management.OsManagementClient", "scheduled_job")
-        DeleteAny(config, signer, processCompartments, "os_management.OsManagementClient", "software_source")
+        # print_header("Deleting OS Management services at " + CurrentTimeString() + "@ " + region, 1)
+        # DeleteAny(config, signer, processCompartments, "os_management.OsManagementClient", "managed_instance_group")
+        # DeleteAny(config, signer, processCompartments, "os_management.OsManagementClient", "scheduled_job")
+        # DeleteAny(config, signer, processCompartments, "os_management.OsManagementClient", "software_source")
 
         print_header("Deleting Compute Instances at " + CurrentTimeString() + "@ " + region, 1)
         DeleteAny(config, signer, processCompartments, "core.ComputeManagementClient", "instance_pool", DeleteCommand="terminate_instance_pool", DelState="TERMINATED", DelingSate="TERMINATING")
